fbf4312551a03283c6c5b97a70e789286469aab2
Rasmus Göransson <1162652+rasmusgo@users.noreply.github.com>
2021-10-25 21:37:06 +0200

    Remove persistence/serialization of app

--- src/app.rs
+++ src/app.rs
@@ -39,29 +39,17 @@ const SPRINGS: [Spring; 3] = [
     },
 ];
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
-#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 pub struct StiffPhysicsApp {
     point_mass: f32,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     points: Vec<Point2<f64>>,
     springs: Vec<Spring>,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     relaxed_points: Vec<Point2<f64>>,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     simulation_state: DVector<f64>,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     exp_a_sim_step: DMatrix<f64>,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     exp_a_audio_step: DMatrix<f64>,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     enable_simulation: bool,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     pub audio_player: Arc<Mutex<Option<anyhow::Result<AudioPlayer>>>>,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     audio_history: Vec<(f32, f32)>,
-    #[cfg_attr(feature = "persistence", serde(skip))]
     audio_history_index: usize,
 }
 
@@ -111,19 +99,6 @@ impl epi::App for StiffPhysicsApp {
         _frame: &mut epi::Frame<'_>,
         _storage: Option<&dyn epi::Storage>,
     ) {
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        #[cfg(feature = "persistence")]
-        if let Some(storage) = _storage {
-            *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
-        }
-    }
-
-    /// Called by the frame work to save state before shutdown.
-    /// Note that you must enable the `persistence` feature for this to work.
-    #[cfg(feature = "persistence")]
-    fn save(&mut self, storage: &mut dyn epi::Storage) {
-        epi::set_value(storage, epi::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
